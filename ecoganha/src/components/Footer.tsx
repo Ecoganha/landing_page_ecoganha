7e7f38ca9956a2d264dbf416ad8c3340e640612f
Samuel Santos <samuel_a_santos@hotmail.com>
2025-03-21 15:39:37 -0300

    :sparkles: feat(footer): Componente Footer finalizado
    Footer com identidade da marca, mensagem institucional e direitos autorais. Estilização com fundo temático e elementos visuais como o ícone de coração.
    
    Reviewed-by: Samuel Santos

--- ecoganha/src/components/Footer.tsx
+++ ecoganha/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Footer
-            </h1>
+        <div className="text-center text-white -mt-10 flex flex-col p-10 pb-10 bg-[#003F25] ">
+            <p className="text-xl font-bold">EcoGanha</p>
+            <p className="text-md font-bold p-2">Feito com <img className="inline" src=".\src\assets\heart.png" /> para um lugar mais verde</p>
+            <p className="text-sm mt-3">© Copyright EcoGanha All Rights Reserved</p>
         </div>
     )
 }
