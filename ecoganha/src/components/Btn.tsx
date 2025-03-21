9f824a24256435f9c93fb98ffe7ceb3c7f03e97f
Samuel Santos <samuel_a_santos@hotmail.com>
2025-03-21 15:28:07 -0300

    :sparkles: feat(cta): Componente Cta implementado
    Seção de chamada para ação finalizada com título impactante, texto descritivo e botão reutilizável. Estilização com sombra e responsividade para engajamento do usuário.
    
    Reviewed-by: Samuel Santos

---
+++ ecoganha/src/components/Btn.tsx
@@ -0,0 +1,5 @@
+export const Btn = () => {
+    return (
+        <button className="px-8 py-2 mx-auto mt-5 font-bold bg-[#003F25] flex text-center text-white rounded-full cursor-pointer hover:bg-[#FBCA27] hover:text-[#003F25]">cadastrar</button>
+    )
+}
--- ecoganha/src/components/Cta.tsx
+++ ecoganha/src/components/Cta.tsx
@@ -1,9 +1,13 @@
+import { Btn } from "./Btn"
+
 export const Cta = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Cta
-            </h1>
+        <div className="text-center bg-gray-50 flex flex-col items-center justify-center w-full my-10 py-10 px-5 gap-8 mx-auto shadow-[0px_8px_18px_0px_#00000026]">
+            <h2 className="text-2xl font-bold text-[#003F25]">
+                Cadastre-se Agora e Comece a Ganhar!
+            </h2>
+            <p className="text-lg text-[#003F25]">Não fique de fora dessa transformação! Inscreva-se gratuitamente e receba em primeira mão quando for lançado. Faça parte da revolução sustentável.</p>
+            <Btn />
         </div>
     )
 }
