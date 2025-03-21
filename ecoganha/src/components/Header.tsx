45dabb90e3b9fd87890b1e5023e041a7cc6cfa2c
Samuel Santos <samuel_a_santos@hotmail.com>
2025-03-21 16:28:08 -0300

    :sparkles: feat(header): Responsividade para desktop
    Adicionada responsividade ao componente Header para telas maiores. Ajustado o layout com `flex`, tamanhos de fonte adaptativos e posicionamento do botão para melhor experiência em desktop.
    
    Reviewed-by: Samuel Santos

--- ecoganha/src/components/Header.tsx
+++ ecoganha/src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { Btn } from "./Btn"
+
 export const Header = () => {
     return (
-        <div>
-            <div className="text-2xl font-bold bg-[#FBCA27] p-4 mx-5 my-7 rounded-full text-center drop-shadow-lg">
-                <h1 className="text-[#003F25]">Eco Ganha</h1>
+        <div className="p-6">
+            <div className="text-2xl font-bold bg-[#FBCA27] p-4 mx-5 rounded-full text-center drop-shadow-lg max-w-[1280px] md:mx-auto md:text-3xl md:text-left md:flex md:justify-between md:items-center">
+                <h1 className="text-[#003F25] md:ml-11 md:text-2xl">Eco Ganha</h1>
+                <div className="hidden md:block md:text-xl md: mr-11">
+                    <Btn />
+                </div>
             </div>
         </div>
     )
--- ecoganha/src/components/Hero.tsx
+++ ecoganha/src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Btn } from "./Btn"
 export const Hero = () => {
     return (
         <div className="text-center flex flex-col justify-center mt-9 mb-4">
-            <img src=".\src\assets\img_hero.png" alt="imagem de funções no celular" />
+            <img className="mx-auto" src=".\src\assets\img_hero.png" alt="imagem de funções no celular" />
             <p className="-mt-20 mx-11 text-2xl font-bold text-[#003F25]">Junte-se ao Ecoganha:<br /> Recicle e Ganhe Recompensas!</p>
             <p className="mx-11 my-4 text-xl text-[#003F25]">O Ecoganha transforma o descarte correto de recicláveis em benefícios reais. Recicle, acumule pontos e troque por produtos sustentáveis!</p>
             <Btn />
