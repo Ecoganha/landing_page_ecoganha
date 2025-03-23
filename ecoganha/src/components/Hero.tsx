33c64511ffe713acaf6dcaaee1ab0c979d38424c
Samuel Santos <samuel_a_santos@hotmail.com>
2025-03-22 22:31:43 -0300

    :sparkles: feat(hero): Ajustes finais da responsividade para desktop
    Melhorado o layout do Hero para telas maiores. Ajustado tamanho da imagem, espaçamentos e alinhamento dos elementos com foco em legibilidade e usabilidade em resoluções amplas.
    
    Reviewed-by: Samuel Santos

--- ecoganha/src/components/Hero.tsx
+++ ecoganha/src/components/Hero.tsx
@@ -3,10 +3,10 @@ import { Btn } from "./Btn"
 export const Hero = () => {
     return (
         <div className="text-center flex flex-col justify-center mt-9 mb-4 max-w-[1280px] md:mx-auto md:flex-row">
-            <img className="mx-auto md:order-2 md:pt-20 md:flex-1 md:mr-10" src=".\src\assets\img_hero.png" alt="imagem de funções no celular" />
+            <img className="mx-auto md:order-2 md:pt-20 md:flex-1 md:mr-10 md:w-sm" src=".\src\assets\img_hero.png" alt="imagem de funções no celular" />
             <div className="md:order-1 md:flex md:flex-col md:justify-center md:flex-2 md:items-start md:text-left md:max-w-[900px] md:ml-10">
                 <p className="-mt-20 mx-11 text-2xl font-bold text-[#003F25] md:m-0">Junte-se ao Ecoganha:<br /> Recicle e Ganhe Recompensas!</p>
-                <p className="mx-11 my-4 text-xl text-[#003F25] md:mx-0 md:my-6">O Ecoganha transforma o descarte correto de recicláveis em benefícios reais. Recicle, acumule pontos e troque por produtos sustentáveis!</p>
+                <p className="mx-11 my-4 text-xl text-[#003F25] md:mx-0 md:my-8">O Ecoganha transforma o descarte correto de recicláveis em benefícios reais. Recicle, acumule pontos e troque por produtos sustentáveis!</p>
                 <div>
                     <Btn />
                 </div>
